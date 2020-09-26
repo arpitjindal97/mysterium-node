ab82b10cb0240c4a174fc38eb849daa9f8c12237
Arpit Agarwal <arpit.agarwal97@protonmail.com>
2020-09-26 21:05:58 +0530

    Add github action workflow
    Docker images will be build and pushed

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+echo "Logging into Docker Registry"
+echo $DOCKER_TOKEN | docker login --username arpitjindal1997 --password-stdin
+
+echo "Starting to clone secrets repo"
+git clone https://arpitjindal97:$GIT_PAT_TOKEN@github.com/arpitjindal97/secrets.git
+
+mkdir db
+cp secrets/myst/myst.db db/
+
+nodeIdentities=($(ls secrets/myst | grep 0x))
+
+for i in "${nodeIdentities[@]}"
+do
+    tag="${i:2:7}"
+    echo "Building Docker Image of $i" with tag "$tag"
+    rm -rf keystore 2> /dev/null
+    cp -a "secrets/myst/$i/keystore" .
+    docker buildx build --no-cache --platform linux/arm64,linux/amd64 -t arpitjindal1997/myst-multiarch:$tag . --push
+done
+
+if rm -r secrets db keystore 2> /dev/null;
+then
+    echo "SUCCESS"
+else
+    echo "FAILED"
+fi

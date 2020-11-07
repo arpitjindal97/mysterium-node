0ff57169bb57062ff2c091666b4a60c1d0d139ad
Arpit Agarwal <arpit.agarwal97@protonmail.com>
2020-11-07 22:48:24 +0530

    Add ipsec client config

--- run.sh
+++ run.sh
@@ -2,12 +2,17 @@
 
 chmod 666 /dev/net/tun
 
-echo $USER > /vpn/pass.txt
-echo $PASS >> /vpn/pass.txt
-
-openvpn --config /vpn/ovpn-config/$FILE --auth-user-pass /vpn/pass.txt &
-
+if [ -n $SERVER ];
+then
+    echo "$USER : EAP \"$PASS\""  > /etc/ipsec.secrets​
+    sed -i "s/USER/$USER/g" /etc/ipsec.conf
+    sed -i "s/SERVER/$SERVER/g" /etc/ipsec.conf
+    ipsec start
+else
+    echo $USER > /vpn/pass.txt
+    echo $PASS >> /vpn/pass.txt
+    openvpn --config /vpn/ovpn-config/$FILE --auth-user-pass /vpn/pass.txt --daemon
+fi
 sleep 10
 
 /usr/local/bin/docker-entrypoint.sh service --agreed-terms-and-conditions
-
